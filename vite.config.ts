93cca1d6fc1b0b3a469ccaa8b38596337008ffc2
Ümit Tunç <umitreva@gmail.com>
2026-06-21 01:43:27 +0300

    chore: initialize project setup and configuration

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import electron from 'vite-plugin-electron'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    electron([
+      {
+        entry: 'src/main/main.ts',
+        vite: {
+          build: {
+            outDir: 'dist-electron',
+          },
+        },
+      },
+      {
+        entry: 'src/main/preload.ts',
+        onclean(options) {
+          options.clean()
+        },
+        vite: {
+          build: {
+            outDir: 'dist-electron',
+          },
+        },
+      },
+    ]),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/renderer'),
+    },
+  },
+})
